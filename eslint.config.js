7bfd5cf1211964a6484c47dc52121810116f12a9
keisuke3 <ishii@gizumo-inc.jp>
2024-07-28 17:09:36 +0900

    chore: ESLint設定

---
+++ eslint.config.js
@@ -0,0 +1,136 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintImport from "eslint-plugin-import"
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...eslintPluginAstro.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      eslintImport,
+      "unused-imports": unusedImports,
+    },
+    languageOptions: {
+      parserOptions: {
+        "project": "./tsconfig.json"
+      },
+    },
+    rules: {
+      "eslintImport/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "object",
+            "type",
+            "index"
+          ],
+          "newlines-between": "always",
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "alphabetize": {
+            "order": "asc",
+            "caseInsensitive": true
+          },
+          "pathGroups": [
+            {
+              "pattern": "@/layouts/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/pages/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/components/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/utils/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/types/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/styles/**",
+              "group": "internal",
+              "position": "before"
+            }
+          ]
+        }
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+      "eslintImport/no-default-export": "error",
+      "eslintImport/no-cycle": "error",
+      "eslintImport/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "js": "never",
+          "jsx": "never",
+          "ts": "never",
+          "tsx": "never",
+        }
+      ],
+      "eslintImport/prefer-default-export": "off",
+      "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/restrict-plus-operands": [
+        "error",
+        {
+          "skipCompoundAssignments": true,
+          "allowBoolean": false,
+          "allowNullish": false,
+          "allowNumberAndString": false,
+          "allowRegExp": false,
+          "allowAny": false
+        }
+      ],
+      "@typescript-eslint/require-array-sort-compare": [
+        "error",
+        {
+          "ignoreStringArrays": true
+        }
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          "selector": "typeLike",
+          "format": ["PascalCase"]
+        }
+      ],
+      "no-implicit-coercion": "error",
+      "unused-imports/no-unused-imports": "error",
+      "arrow-body-style": "off",
+      "no-console": "error",
+      "no-fallthrough": "off",
+      "camelcase": [
+        "error",
+        {
+          "ignoreImports": true,
+          "properties": "never",
+          "ignoreDestructuring": true
+        }
+      ]
+    }
+  }
+);
