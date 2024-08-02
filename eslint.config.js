dc337e87301316e54879438453babcb50eb92f88
keisuke3 <ishii@gizumo-inc.jp>
2024-08-02 11:20:22 +0900

    chore: Prettier導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import eslint from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintImport from "eslint-plugin-import";
-import unusedImports from "eslint-plugin-unused-imports";
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintImport from 'eslint-plugin-import'
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   eslint.configs.recommended,
@@ -9,66 +9,66 @@ export default [
   {
     plugins: {
       eslintImport,
-      "unused-imports": unusedImports,
+      'unused-imports': unusedImports,
     },
     rules: {
-      "eslintImport/order": [
-        "error",
+      'eslintImport/order': [
+        'error',
         {
-          "groups": [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "object",
-            "type",
-            "index"
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'object',
+            'type',
+            'index',
           ],
-          "newlines-between": "always",
-          "pathGroupsExcludedImportTypes": ["builtin"],
-          "alphabetize": {
-            "order": "asc",
-            "caseInsensitive": true
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
           },
-          "pathGroups": [
+          pathGroups: [
             {
-              "pattern": "/pages/**",
-              "group": "internal",
-              "position": "before"
+              pattern: '/pages/**',
+              group: 'internal',
+              position: 'before',
             },
             {
-              "pattern": "/styles/**",
-              "group": "internal",
-              "position": "before"
-            }
-          ]
-        }
+              pattern: '/styles/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+        },
       ],
-      "eslintImport/no-cycle": "error",
-      "eslintImport/extensions": [
-        "error",
-        "ignorePackages",
+      'eslintImport/no-cycle': 'error',
+      'eslintImport/extensions': [
+        'error',
+        'ignorePackages',
         {
-          "js": "never",
-          "jsx": "never",
-          "ts": "never",
-          "tsx": "never",
-        }
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
       ],
-      "eslintImport/prefer-default-export": "off",
-      "no-implicit-coercion": "error",
-      "unused-imports/no-unused-imports": "error",
-      "arrow-body-style": "off",
-      "no-console": "error",
-      "no-fallthrough": "off",
-      "camelcase": [
-        "error",
+      'eslintImport/prefer-default-export': 'off',
+      'no-implicit-coercion': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'arrow-body-style': 'off',
+      'no-console': 'error',
+      'no-fallthrough': 'off',
+      camelcase: [
+        'error',
         {
-          "ignoreImports": true,
-          "properties": "never",
-          "ignoreDestructuring": true
-        }
-      ]
-    }
-  }
+          ignoreImports: true,
+          properties: 'never',
+          ignoreDestructuring: true,
+        },
+      ],
+    },
+  },
 ]
