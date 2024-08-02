24d0827ece7592abafe2112f5eb940a68cd94cbc
keisuke3 <ishii@gizumo-inc.jp>
2024-08-02 17:57:45 +0900

    chore: plop導入

---
+++ plopfile.mjs
@@ -0,0 +1,29 @@
+export default function (plop) {
+  plop.setGenerator('component', {
+    description: 'コンポーネント作成',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: '作成するページのパスを入力してください',
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/{{lowerCase name}}/index.html",
+        templateFile: ".plopfile/index.html.hbs",
+      },
+      {
+        type: "add",
+        path: "src/{{lowerCase name}}/index.css",
+        templateFile: ".plopfile/index.css.hbs",
+      },
+      {
+        type: "add",
+        path: "src/{{lowerCase name}}/index.js",
+        templateFile: ".plopfile/index.js.hbs",
+      }
+    ],
+  })
+}
